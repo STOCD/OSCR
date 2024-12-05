6f920dbf6c6d8ddb36e3c58425926f4bb3812a11
Kraust <secretdragoon@gmail.com>
2024-12-05 14:48:34 -0500

    Added Winter Invasion detection.

--- OSCR/main.py
+++ OSCR/main.py
@@ -13,7 +13,7 @@ ignored_abilities = [
 
 
 class OSCR:
-    version = "2024.12.04.1"
+    version = "2024.12.05.1"
 
     def __init__(self, log_path: str = None, settings: dict = None):
         self.log_path = log_path
