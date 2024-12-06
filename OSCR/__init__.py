fe141c25f02941b49a25404a1ee58d15723b82dd
Shinga13 <93780215+Shinga13@users.noreply.github.com>
2024-12-06 17:08:17 +0100

    Fixed multiprocessing issue

--- OSCR/__init__.py
+++ OSCR/__init__.py
@@ -7,11 +7,3 @@ from .main import OSCR
 __all__ = (
         'HEAL_TREE_HEADER', 'LIVE_TABLE_HEADER', 'LiveParser', 'OSCR', 'repair_logfile',
         'split_log_by_combat', 'split_log_by_lines', 'TABLE_HEADER', 'TREE_HEADER', 'TreeItem')
-
-# configure multiprocessing
-if __name__ == 'OSCR':
-    from multiprocessing import freeze_support, set_start_method
-    freeze_support()
-    set_start_method('spawn')
-    del freeze_support
-    del set_start_method
