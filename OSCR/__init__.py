6d57943b394bd7bd17a76a269ff1bb212a63deeb
Shinga13 <93780215+Shinga13@users.noreply.github.com>
2026-03-14 19:53:01 +0100

    adding DetectionInfo to package exports

--- OSCR/__init__.py
+++ OSCR/__init__.py
@@ -1,9 +1,9 @@
 from .constants import HEAL_TREE_HEADER, LIVE_TABLE_HEADER, TABLE_HEADER, TREE_HEADER
-from .datamodels import TreeItem
+from .datamodels import DetectionInfo, TreeItem
 from .iofunc import compose_logfile, extract_bytes, repair_logfile
 from .liveparser import LiveParser
 from .main import OSCR
 
 __all__ = (
-        'compose_logfile', 'extract_bytes', 'HEAL_TREE_HEADER', 'LIVE_TABLE_HEADER', 'LiveParser',
-        'OSCR', 'repair_logfile', 'TABLE_HEADER', 'TREE_HEADER', 'TreeItem')
+    'compose_logfile', 'DetectionInfo', 'extract_bytes', 'HEAL_TREE_HEADER', 'LIVE_TABLE_HEADER',
+    'LiveParser', 'OSCR', 'repair_logfile', 'TABLE_HEADER', 'TREE_HEADER', 'TreeItem')
